ba0134642806d1e21002ffc7f03460e34b3faf11
jainsameer1991 <jainsameer20@gmail.com>
2017-02-18 21:16:37 +0530

    ROA-2SC Create MVC architecture for ROA project
    Reformat code to assign controlling functionality to ScoreboardController from View

--- RoadFighter/app/src/main/java/com/chopsy/roadfighter/controller/ScoreboardController.java
+++ RoadFighter/app/src/main/java/com/chopsy/roadfighter/controller/ScoreboardController.java
@@ -1,13 +1,42 @@
 package com.chopsy.roadfighter.controller;
 
+import android.os.Handler;
+
 import com.chopsy.roadfighter.view.ScoreboardView;
 
 public class ScoreboardController {
 
     private ScoreboardView mScoreboardView;
+    private Handler mHandler;
+    private int mTime;
+    private int mDistanceCovered;
+    private int mCurrentSpeed;
 
-    public ScoreboardController(ScoreboardView scoreboardView){
+    public ScoreboardController(ScoreboardView scoreboardView) {
         mScoreboardView = scoreboardView;
-       GameContext.registerScoreboardController(this);
+        GameContext.registerScoreboardController(this);
+        mHandler = new Handler();
+        mHandler.postDelayed(mAction, 1000);
+    }
+
+    Runnable mAction = new Runnable() {
+        @Override
+        public void run() {
+            mTime++;
+            mScoreboardView.reDraw();
+            mHandler.postDelayed(this, 1000);
+        }
+    };
+
+    public int getTime() {
+        return mTime;
+    }
+
+    public int getDistanceCovered() {
+        return mDistanceCovered;
+    }
+
+    public int getCurrentSpeed() {
+        return mCurrentSpeed;
     }
 }
